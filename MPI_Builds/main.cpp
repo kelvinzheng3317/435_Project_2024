a3ffab6e87cc4c6ed6e32f04f5f6439d45b4fe2e
Kelvin Zheng <kelvinzheng21@gmail.com>
2024-10-07 16:52:31 -0500

    added basic MPI code to main

---
+++ MPI_Builds/main.cpp
@@ -0,0 +1,64 @@
+#include "mpi.h"
+
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+#include <string.h>
+
+
+int main(int argc, char* argv[]) {
+  int arrSize = 64;
+  string arrType = "sorted"; // sorted, onePercent, random, reverseSorted
+  string sortType = "bitonic" // bitonic, merge, sample, radix
+
+  if (argc == 3) {
+    arrSize = atoi(argv[1]);
+    arrVals = argv[2];
+  }
+
+
+  int rank;
+  int num_procs;
+
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&num_procs);
+
+  if (rank == 0) {
+    // initialize array
+    int data[arrSize];
+
+    if (arrType == "sorted") {
+      for (int i=0; i<arrSize; ++i) {
+        data[i] = i;
+      }
+    } else if (arrType == "random")
+    {
+      for (int i = 0; i < arrSize; i++) {
+        data[i] = rand() % 100;
+      }
+    }
+    
+  }
+
+  int local_size = arrSize / num_procs;
+  int local_arr[local_size];
+
+  MPI_Scatter(&data, local_size, MPI_INT, &local_arr, local_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+  // Do the sort
+
+  MPI_Gather(local_arr, local_size, MPI_INT, &data, local_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+  // print out results to confirm that the array is correctly sorted
+  if (rank == 0) {
+    cout << "Sorted array: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << data[i] << ", ";
+    }
+    cout << endl;
+  }
+
+  MPI_Finalize();
+}
