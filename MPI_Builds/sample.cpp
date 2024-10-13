4cb393830d8e10ce613e9f9a926eebe4451ee9df
B.J <byjong@tamu.edu>
2024-10-13 02:01:38 -0500

    function to print array

--- MPI_Builds/sample.cpp
+++ MPI_Builds/sample.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <iostream>
 
+// For data generation or randomized array
+#include <cstdlib>
+#include <ctime>
+
 // For performance analysis, not used currently
 #include <caliper/cali.h>
 #include <caliper/cali-manager.h>
@@ -11,6 +15,14 @@ using std::vector;
 using std::cout, std::endl;
 using std::sort;
 
+void printArray(const vector<int>& arr, int rank, const std::string& step) {
+    cout  << "Rank" << rank << " " << step << ": ";
+    for (int elem : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
@@ -18,9 +30,18 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int arr_size = 16;
-    vector<int> main_arr;
+    const int arrSize = 16;
+    vector<int> mainArr;
 
-    MPI
+    if (rank == 0) {
+        std::rand(std::time(0));
+        mainArr.resize(arrSize);
+        for (int i = 0; i < arrSize; ++i) {
+            mainArr[i] = std::rand() % 100;    // Fill array with numbers between 0 and 99
+        }
+        printArray(mainArr, rank, "Initial Array");
+    }
+
+    
     return 0;
 }
