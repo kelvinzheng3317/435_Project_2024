c0d0fec3515b2e9f016805d0d4e42cfaf8f712e0
B.J <byjong@tamu.edu>
2024-10-16 16:17:22 -0500

    working sample sort

--- MPI_Builds/sample.cpp
+++ MPI_Builds/sample.cpp
@@ -4,20 +4,13 @@
 #include <iostream>
 #include <numeric>
 
-#include <caliper/cali.h>
-#include <caliper/cali-manager.h>
-#include <adiak.hpp>
-
-// For data generation or randomized array
-#include <cstdlib>
-#include <ctime>
-
 #include "data.cpp"
 
 using namespace std;
 
+// Function to print array
 void printArray(const vector<int>& arr, int rank, const string& step) {
-    cout  << "Rank " << rank << " - " << step << ": ";
+    cout << "Rank " << rank << " - " << step << ": ";
     for (int elem : arr) {
         cout << elem << " ";
     }
@@ -25,121 +18,152 @@ void printArray(const vector<int>& arr, int rank, const string& step) {
 }
 
 int main(int argc, char* argv[]) {
-    CALI_CXX_MARK_FUNCTION;
+    int initSize = 0;
+    string arrType = ""; 
 
-    int initSize = 64;
-    string arrType = "random"; // sorted, perturbed, random, reverse
-    string sortType = "sample"; // bitonic, merge, sample, radix
-
+    // Take in command-line arguments
     if (argc == 3) {
         initSize = atoi(argv[1]);
         arrType = argv[2];
     }
 
-    cali::ConfigManager mgr;
-    mgr.start();
-    
-    double sort_start, sort_end;
-
     int rank, num_procs;
-
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
     vector<int> mainArr;
-    
+
+    // Master process generate initial array
     if (rank == 0) {
         mainArr.resize(initSize);
         generateArray(mainArr.data(), arrType, initSize);
         printArray(mainArr, rank, "Initial Array");
-
-        CALI_MARK_BEGIN("Sample Sort");
-        sort_start = MPI_Wtime();
     }
 
     int localSize = initSize / num_procs;
     vector<int> localArr(localSize);
     
-    // 1. Scatter chunks of main array to all processes
+    // 1. Scatter chunks of initial array to all processes
     MPI_Scatter(mainArr.data(), localSize, MPI_INT, localArr.data(), localSize, MPI_INT, 0, MPI_COMM_WORLD);
 
     // 2. Locally sort each chunk
     sort(localArr.begin(), localArr.end());
-    
+
     // 3. Select pivots
-    /* NOTE: samples.[i * num_procs / (num_procs + 1)] can be changed to get different set of samples.
-    Chose [i * num_procs / (num_procs + 1)] for evenlt distributed selection. */
+    /* NOTE: 
+    - samples.[i * num_procs / (num_procs + 1)] can be changed to get different set of samples.
+    - Chose [i * num_procs / (num_procs + 1)] for evenly distributed selection. */
 
     vector<int> pivots(num_procs - 1);
     if (rank == 0) {
-        vector<int> samples;
+        vector<int> samples(num_procs);
         for (int i = 0; i < num_procs; ++i) {
-            samples.push_back(mainArr[i * (initSize / num_procs)]);
+            samples[i] = mainArr[i * (initSize / num_procs)];
         }
         sort(samples.begin(), samples.end());
         for (int i = 1; i < num_procs; ++i) {
-            pivots[i - 1] = samples[i * num_procs / (num_procs + 1)]; //NOTE
+            pivots[i - 1] = samples[i * num_procs / (num_procs + 1)];   //NOTE
         }
     }
+
     MPI_Bcast(pivots.data(), num_procs - 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     // 4. Bucketing based on pivots
     vector<vector<int>> buckets(num_procs);
     for (int i = 0; i < localSize; ++i) {
         int bucket_no = 0;
-        while ((bucket_no < num_procs - 1) && (localArr[i] > pivots[bucket_no])) {
+        while (bucket_no < num_procs - 1 && localArr[i] > pivots[bucket_no]) {
             ++bucket_no;
         }
         buckets[bucket_no].push_back(localArr[i]);
     }
 
-    // 5. Prepare send and receive buffers for bucket data
-    vector<int> sendCounts(num_procs), recvCounts(num_procs);
+    for (int i = 0; i < num_procs; ++i) {
+        printArray(buckets[i], rank, "Bucket " + to_string(i));
+    }
+
+    // 5. Calculate necessary values for send/recv buffers
+
+    // Calculate counts of elements for sending buffer from each bucket
+    vector<int> sendCounts(num_procs);
     for (int i = 0; i < num_procs; ++i) {
         sendCounts[i] = buckets[i].size();
     }
-    MPI_Alltoall(sendCounts.data(), 1, MPI_INT, recvCounts.data(), 1, MPI_INT, MPI_COMM_WORLD);
 
-    // Calculating displacements for for send/recv
-    vector<int> sendDispls(num_procs), recvDispls(num_procs);
-    partial_sum(sendCounts.begin(), sendCounts.end() - 1, sendDispls.begin() + 1);
-    partial_sum(recvCounts.begin(), recvCounts.end() - 1, recvDispls.begin() + 1);
+    // Calculate displacement for sending buffer
+    vector<int> sendDispls(num_procs);
+    sendDispls[0] = 0;
+    for (int i = 1; i < num_procs; ++i) {
+        sendDispls[i] = sendDispls[i - 1] + sendCounts[i - 1];
+    }
 
-    // Flatten buckets into single send buffer
+    // Prepare send buffer
     vector<int> sendBuffer(accumulate(sendCounts.begin(), sendCounts.end(), 0));
     for (int i = 0; i < num_procs; ++i) {
         copy(buckets[i].begin(), buckets[i].end(), sendBuffer.begin() + sendDispls[i]);
     }
 
-    // Receive buffer for the redistributed data
+    printArray(sendBuffer, rank, "Send Buffer");
+
+    // Calculate counts of elements received for receive buffer 
+    vector<int> recvCounts(num_procs);
+    MPI_Alltoall(sendCounts.data(), 1, MPI_INT, recvCounts.data(), 1, MPI_INT, MPI_COMM_WORLD);
+
+    // Calculate disolacement for receive buffer
+    vector<int> recvDispls(num_procs);
+    recvDispls[0] = 0;
+    for (int i = 1; i < num_procs; ++i) {
+        recvDispls[i] = recvDispls[i - 1] + recvCounts[i - 1];
+    }
+
+    // Prepare receive receive buffer
     vector<int> recvBuffer(accumulate(recvCounts.begin(), recvCounts.end(), 0));
 
-    // 6. Redistribute the data to all processes
-    MPI_Alltoallv(sendBuffer.data(), sendCounts.data(), sendDispls.data(), MPI_INT,
-                recvBuffer.data(), recvCounts.data(), recvDispls.data(), MPI_INT, MPI_COMM_WORLD);
+    if (rank == 0) {
+        cout << "Rank " << rank << " - recvCounts: ";
+        for (int count : recvCounts) {
+            cout << count << " ";
+        }
+        cout << endl;
+    }
 
-    // 7. Sort all received buckets
+    // Exchange send/recv data between all processes
+    MPI_Alltoallv(sendBuffer.data(), sendCounts.data(), sendDispls.data(), MPI_INT,
+                   recvBuffer.data(), recvCounts.data(), recvDispls.data(), MPI_INT, MPI_COMM_WORLD);
+
+    printArray(recvBuffer, rank, "Received Buffer after Alltoallv");
+
+    // Sort receive buffer locally
     sort(recvBuffer.begin(), recvBuffer.end());
 
-    // 8. Gather sorted received subarrays (buckets) to MASTER process and print
-    vector<int> finalArr;
+    // Hold sizes of receive buffer from each rank
+    vector<int> finalRecvCounts(num_procs);
+
+    // Gather receive buffer sizes from all ranks 
+    int recvBufferSize = recvBuffer.size();
+    MPI_Gather(&recvBufferSize, 1, MPI_INT, finalRecvCounts.data(), 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Calculate displacements for final receive buffers
+    vector<int> finalRecvDispls(num_procs);
     if (rank == 0) {
-        finalArr.resize(initSize);
+        finalRecvDispls[0] = 0;
+        for (int i = 1; i < num_procs; ++i) {
+            finalRecvDispls[i] = finalRecvDispls[i - 1] + finalRecvCounts[i - 1];
+        }
     }
-    MPI_Gatherv(recvBuffer.data(), recvBuffer.size(), MPI_INT, finalArr.data(), recvCounts.data(), recvDispls.data(), MPI_INT, 0, MPI_COMM_WORLD);
 
+    // 6. Gather all sorted received buffers into final array
+    vector<int> finalArr(initSize);
+    MPI_Gatherv(recvBuffer.data(), recvBuffer.size(), MPI_INT, finalArr.data(), finalRecvCounts.data(), finalRecvDispls.data(), MPI_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        sort_end = MPI_Wtime();
-        CALI_MARK_END("Sample Sort");
-
+        printArray(finalArr, rank, "Final Array Before Sorting Output");
         cout << "Final sorted array: ";
         for (int i = 0; i < initSize; ++i) {
             cout << finalArr[i] << " ";
         }
         cout << endl;
-        cout  << "Total sorting time: " << sort_end - sort_start << "secs" << endl;
     }
 
     MPI_Finalize();
